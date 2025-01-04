dd855b4d3560a931f8f222cfadfffbf6996f88a3
salthareket <info@salthareket.com>
2025-01-04 17:55:04 +0300

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -2,7 +2,8 @@
 
 require_once dirname(__DIR__, 6) . '/wp-load.php'; // WordPress'i yükle
 
-require_once 'src/update.php';
+//require_once 'src/update.php';
+require_once dirname(__DIR__, 3) . "/install/update.php";
 require_once 'src/plugin-manager.php';
 require      "src/variables.php";
 require_once "src/plugins.php";
