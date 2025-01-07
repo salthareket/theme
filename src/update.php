f18912662f8c1b19e1d432a655c2a676b238a5e5
salthareket <info@salthareket.com>
2025-01-07 23:53:12 +0300

    Update update.php

--- src/update.php
+++ src/update.php
@@ -31,7 +31,7 @@ class Update {
         ["id" => "copy_fields", "name" => "Copying ACF Fields"],
         ["id" => "register_fields", "name" => "Registering ACF Fields"],
         ["id" => "update_fields", "name" => "Updating ACF Fields"],
-        ["id" => "npm_packages", "name" => "npm packages copying"],
+        //["id" => "npm_packages", "name" => "npm packages copying"],
         ["id" => "npm_install", "name" => "npm packages installing"],
         ["id" => "compile_methods", "name" => "Compile Frontend & Admin Methods"],
         ["id" => "generate_files", "name" => "Generating Files"],
@@ -860,11 +860,11 @@ class Update {
             acf_save_post_block_columns_action( $post_id );
         }
     }
-    private static function npm_packages(){
+    /*private static function npm_packages(){
         $source = SH_PATH . 'package.json';
         $destination = get_home_path();
         self::fileCopy($source, $destination);
-    }
+    }*/
     private static function npm_install(): string{
         $workingDir = ABSPATH;
         if (!is_dir($workingDir)) {
@@ -969,11 +969,11 @@ class Update {
                     self::update_task_status('install_local_plugins', true);
                     wp_send_json_success(['message' => 'Local plugins installed successfully']);
                     break;
-                case 'npm_packages':
+                /*case 'npm_packages':
                     self::npm_packages();
                     self::update_task_status('npm_packages', true);
                     wp_send_json_success(['message' => 'NPM Packages copied successfully']);
-                    break;
+                    break;*/
                 case 'npm_install':
                     self::npm_install();
                     self::update_task_status('npm_install', true);
