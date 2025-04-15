2195220f073764a7008ee1a7b5e4584c1ca30388
salthareket <info@salthareket.com>
2025-04-15 17:58:15 +0300

    update acf to 6.4.0.1

--- src/plugins.php
+++ src/plugins.php
@@ -106,7 +106,7 @@ $GLOBALS["plugins"] = $required_plugins;
 $required_plugins_local = array();
 $required_plugins_local[] = array(
     "type" => ["main"],
-    "v" => "6.3.12",
+    "v" => "6.4.0.1",
     "name" => "advanced-custom-fields-pro/acf.php",
     "file" => "advanced-custom-fields-pro"
 );
