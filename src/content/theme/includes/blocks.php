de22dba7822c20ae7687244d9802572999cdf62e
salthareket <info@salthareket.com>
2025-09-29 23:53:42 +0300

    Refactor theme includes loading and cleanup
    Replaced multiple include statements with include_once in index.php and added a guard to prevent duplicate loading. Removed all code from cron.php, effectively disabling scheduled cleanup actions. Commented out the block renderer filter in blocks.php. Significantly reduced the Project class in project.php, removing most methods and logic, leaving only the constructor and response method.

--- src/content/theme/includes/blocks.php
+++ src/content/theme/includes/blocks.php
@@ -4,4 +4,4 @@ function wporg_block_renderer( $parsed_block, $source_block ) {
     //$parsed_block["attrs"]["disabled"] = "fade-in";
     return $parsed_block;
 }
-add_filter( 'render_block_data', 'wporg_block_renderer', 10, 2 );
+//add_filter( 'render_block_data', 'wporg_block_renderer', 10, 2 );
--- src/content/theme/index.php
+++ src/content/theme/index.php
@@ -1,16 +1,22 @@
 <?php
 
-include "includes/custom-extend.php";
-//include "includes/cron.php";
-//include "includes/rewrite.php";
-include "includes/roles.php";
-///include "includes/blocks.php";
+if (defined('THEME_INCLUDES_LOADED')) {
+    return; // Eğer daha önce yüklendiyse tekrar yükleme
+}
+
+include_once "includes/custom-extend.php";
+include_once "includes/cron.php";
+include_once "includes/rewrite.php";
+include_once "includes/roles.php";
+include_once "includes/blocks.php";
 
 if(ENABLE_MEMBERSHIP){
-	include "includes/my-account.php";
+	include_once "includes/my-account.php";
 }
 if(ENABLE_ECOMMERCE){
-	include "includes/woocommerce.php";
+	include_once "includes/woocommerce.php";
 }
+include_once "includes/project.php";
+include_once "functions.php";
 
-include "functions.php";
+define('THEME_INCLUDES_LOADED', true);
