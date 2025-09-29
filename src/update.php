fc6351b9f560b03ad28a2dcb01ef045af1f46da7
salthareket <info@salthareket.com>
2025-09-30 01:23:01 +0300

    Make disable_heartbeat method static
    Changed the disable_heartbeat method in Update class to static to allow static access and improve code clarity.

--- src/update.php
+++ src/update.php
@@ -87,7 +87,7 @@ class Update {
     }
 
 
-    private function disable_heartbeat($hook){
+    private static function disable_heartbeat($hook){
         if ($hook === 'theme-settings_page_update-theme') {
             wp_deregister_script('heartbeat');
         }
